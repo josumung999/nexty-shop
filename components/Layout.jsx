31d02ca2ffd5b094a69f0fb858fa83a0c77bdf6a
Josué Munganga <jmunganga039@gmail.com>
2022-03-07 14:11:04 +0200

    implemented custom styles for the app

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,23 +1,26 @@
 import { AppBar, Container, Toolbar, Typography } from "@material-ui/core"
 import Head from "next/head"
+import useStyles from "../utils/styles"
 
 const Layout = ({children}) => {
+  const classes = useStyles();
+
   return (
     <div>
       <Head>
         <title>Nexty</title>
       </Head>
-      <AppBar position="static">
+      <AppBar className={classes.navBar} position="static">
         <Toolbar>
           <Typography>
             Nexty Shop
           </Typography>
         </Toolbar>
       </AppBar>
-      <Container>
+      <Container className={classes.main}>
         {children}
       </Container>
-      <footer>
+      <footer className={classes.footer}>
         <Typography>
           © 2022 - Nexty Shop.
         </Typography>
