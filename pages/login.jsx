1dd9319fe4a90d7fffd0a2f9f1e092f4cbb3c41e
Josué <jmunganga039@gmail.com>
2022-03-09 15:18:20 +0200

    Built the login API

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,14 +1,32 @@
 import { Button, Link, List, ListItem, TextField, Typography } from '@material-ui/core';
+import axios from 'axios';
 import NextLink from 'next/link';
+import { useState } from 'react';
 import Layout from '../components/Layout';
 import useStyles from '../utils/styles';
 
 
 export default function Login() {
+  const [ email, setEmail ] = useState('');
+  const [password, setPassword] = useState('');
   const classes = useStyles();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post('/api/users/login', {
+        email,
+        password,
+      });
+      alert('succss login');
+    } catch (err) {
+      alert(err.response.data ? err.response.data.message : err.message);
+    }
+  };
+
   return (
     <Layout title="Login">
-      <form className={classes.form}>
+      <form onSubmit={submitHandler} className={classes.form}>
         <Typography component="h1" variant='h1'>
           Login
         </Typography>
@@ -20,6 +38,7 @@ export default function Login() {
               id='email'
               label='Email'
               inputProps={{ type: 'email' }}
+              onChange={(e) => setEmail(e.target.value)}
             >
             </TextField>
           </ListItem>
@@ -30,6 +49,7 @@ export default function Login() {
               id='password'
               label='Password'
               inputProps={{ type: 'password' }}
+              onChange={(e) => setPassword(e.target.value)}
             >
             </TextField>
           </ListItem>
