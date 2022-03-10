8ad36691a7b636df744fa3a53c3e551726ae6442
Josué <jmunganga039@gmail.com>
2022-03-10 16:09:20 +0200

    Setting up the checkout process

--- utils/Store.js
+++ utils/Store.js
@@ -1,14 +1,20 @@
-import Cookies from "js-cookie";
-import { createContext, useReducer } from "react";
+import Cookies from 'js-cookie';
+import { createContext, useReducer } from 'react';
 
 export const Store = createContext();
 const initialState = {
   darkMode: Cookies.get('darkMode') === 'ON' ? true : false,
   cart: {
-    cartItems: Cookies.get('cartItems')? JSON.parse(Cookies.get('cartItems')) : [],
-    userInfo: Cookies.get('userInfo')
-      ? JSON.parse.userInfo : null,
-  }
+    cartItems: Cookies.get('cartItems')
+      ? JSON.parse(Cookies.get('cartItems'))
+      : [],
+    shippingAddress: Cookies.get('shippingAddress')
+      ? JSON.stringify(Cookies.get('shippingAddress'))
+      : {},
+  },
+  userInfo: Cookies.get('userInfo')
+    ? JSON.userInfo
+    : null,
 };
 
 function reducer(state, action) {
@@ -22,23 +28,31 @@ function reducer(state, action) {
       const existItem = state.cart.cartItems.find(
         (item) => item._id === newItem._id
       );
-      const cartItems = existItem 
-        ? state.cart.cartItems.map((item) => 
-          item._id === existItem._id ? newItem: item
-          ) 
+      const cartItems = existItem
+        ? state.cart.cartItems.map((item) =>
+            item.name === existItem.name ? newItem : item
+          )
         : [...state.cart.cartItems, newItem];
       Cookies.set('cartItems', JSON.stringify(cartItems));
-      return { ...state, cart: { ...state.cart, cartItems } }
+      return { ...state, cart: { ...state.cart, cartItems } };
     }
     case 'CART_REMOVE_ITEM': {
-      const cartItems = state.cart.cartItems.filter(item => item._id !== action.payload._id);
+      const cartItems = state.cart.cartItems.filter(
+        (item) => item._id !== action.payload._id
+      );
       Cookies.set('cartItems', JSON.stringify(cartItems));
-      return { ...state, cart: { ...state.cart, cartItems } }
+      return { ...state, cart: { ...state.cart, cartItems } };
     }
+    case 'SAVE_SHIPPING_ADDRESS':
+      return {
+        ...state,
+        cart: { ...state.cart, shippingAddress: action.payload },
+      };
     case 'USER_LOGIN':
       return { ...state, userInfo: action.payload };
     case 'USER_LOGOUT':
       return { ...state, userInfo: null, cart: { cartItems: [] } };
+
     default:
       return state;
   }
@@ -46,10 +60,6 @@ function reducer(state, action) {
 
 export function StoreProvider(props) {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const value = {state, dispatch}
-  return <Store.Provider value={value}>
-    {props.children}
-  </Store.Provider>
-
-
+  const value = { state, dispatch };
+  return <Store.Provider value={value}>{props.children}</Store.Provider>;
 }
